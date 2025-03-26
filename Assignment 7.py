0155a8e390be389a63b2d66d8b06e46c530cb7e1
aaryan0201 <136610457+aaryan0201@users.noreply.github.com>
2025-03-26 18:20:32 -0400

    Update Assignment 7.py

--- Assignment 7.py
+++ Assignment 7.py
@@ -6,3 +6,33 @@ import pandas as pd
 import dash
 from dash import dcc, html, Input, Output
 import plotly.express as px
+
+url = "https://en.wikipedia.org/wiki/List_of_FIFA_World_Cup_finals"
+tables = pd.read_html(url)
+
+finals_df = None
+for table in tables:
+    cols = [str(col) for col in table.columns]
+    if "Years won" in cols and "Team" in cols and "Years runners-up" in cols:
+        finals_df = table
+        break
+
+country_iso = {
+    "Uruguay": "URY",
+    "Argentina": "ARG",
+    "Italy": "ITA",
+    "Czechoslovakia": "TCH",
+    "Hungary": "HUN",
+    "Brazil": "BRA",
+    "Sweden": "SWE",
+    "Germany": "DEU",
+    "England": "GBR",
+    "Netherlands": "NLD",
+    "France": "FRA",
+    "Spain": "ESP",
+    "Croatia": "HRV"
+}
+finals_df["Team"] = finals_df["Team"].map(country_iso)
+
+print(finals_df)
+
